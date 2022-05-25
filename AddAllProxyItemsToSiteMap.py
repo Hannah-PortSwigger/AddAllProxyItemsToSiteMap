30323f7f652e642611725697e9e86d8e4b92cc57
Hannah-PortSwigger <58562826+Hannah-PortSwigger@users.noreply.github.com>
2022-05-25 11:24:45 +0100

    Allow for only adding in scope items
    Also provides function to implement your own deduplication code

--- AddAllProxyItemsToSiteMap.py
+++ AddAllProxyItemsToSiteMap.py
@@ -2,9 +2,12 @@ from burp import IBurpExtender, IProxyListener
 from java.io import PrintWriter
 
 class BurpExtender(IBurpExtender, IProxyListener):
+    only_in_scope = False
+
     def registerExtenderCallbacks(self, callbacks):
         extName = "Add all proxy items to site map"
         self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
         callbacks.setExtensionName(extName)
         self._stdout = PrintWriter(callbacks.getStdout(), True)
         callbacks.registerProxyListener(self)
@@ -14,5 +17,11 @@ class BurpExtender(IBurpExtender, IProxyListener):
 
     def processProxyMessage(self, messageIsRequest, message):
         if not messageIsRequest:
-            self._callbacks.addToSiteMap(message.getMessageInfo())
+            if ( not self.only_in_scope ) or ( self.only_in_scope and self._callbacks.isInScope( self._helpers.analyzeRequest( message.getMessageInfo() ).getUrl() ) ):
+                self.deduplicate(message)
+        return
+
+    def deduplicate(self, message):
+        # Provide your own implementation here
+        self._callbacks.addToSiteMap(message.getMessageInfo())
         return
