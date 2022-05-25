7d73706f3ac8a3c428c7f9c04a82e416cd565616
Hannah-PortSwigger <58562826+Hannah-PortSwigger@users.noreply.github.com>
2022-05-25 11:29:06 +0100

    adjust white space

--- AddAllProxyItemsToSiteMap.py
+++ AddAllProxyItemsToSiteMap.py
@@ -2,7 +2,10 @@ from burp import IBurpExtender, IProxyListener
 from java.io import PrintWriter
 
 class BurpExtender(IBurpExtender, IProxyListener):
+
+    
     only_in_scope = False
+
 
     def registerExtenderCallbacks(self, callbacks):
         extName = "Add all proxy items to site map"
@@ -20,6 +23,7 @@ class BurpExtender(IBurpExtender, IProxyListener):
             if ( not self.only_in_scope ) or ( self.only_in_scope and self._callbacks.isInScope( self._helpers.analyzeRequest( message.getMessageInfo() ).getUrl() ) ):
                 self.deduplicate(message)
         return
+
 
     def deduplicate(self, message):
         # Provide your own implementation here
